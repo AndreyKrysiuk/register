8c0a879e6e56bb2e413e2f4df51d7ff665f4219a
AndreyKrysyuk <andrey130198@gmail.com>
2018-05-23 19:41:10 +0300

    Person's CRUD was checked, Checking's CRUD not yet

--- register/register/Schemas/Checking.py
+++ register/register/Schemas/Checking.py
@@ -5,9 +5,72 @@ from register.Schemas import Person
 class Checking(Document):
     person_id = ReferenceField(Person)
     solution = StringField(max_length=240)
-    date_accept_ban = DateTimeField()
-    date_refuse_ban = DateTimeField()
+    date_accept_ban = StringField()
+    date_refuse_ban = StringField()
     resolution = StringField(max_length=240)
-    copy_of_request = StringField(max_length=150)
-    copy_of_declaration = StringField(max_length=150)
+
+
+def add_new_checking(person, solution, resolution='-', date_refuse_ban='-',  date_accept_ban='-'):
+    checking = Checking()
+    if (person is None) or (person is not Person):
+        return -1
+    else:
+        checking.person_id = person
+
+    if (solution is None) or (len(solution) > 240):
+        return -1
+    else:
+        checking.solution = solution
+
+    checking.resolution = resolution
+    checking.date_accept_ban = date_accept_ban
+    checking.date_refuse_ban = date_refuse_ban
+
+    checking.save()
+    return 0
+
+
+def get_checking(this_id):
+    checking = Checking.objects.get(id=this_id)
+    if checking is None:
+        return -1
+    else:
+        return checking
+
+
+def get_all_checking():
+    return Checking.objects()
+
+
+def update_checking(this_id, solution, resolution, date_refuse_ban, date_accept_ban):
+
+    if this_id is not None:
+        return -1
+
+    checking = Checking.objects(id=this_id)
+    if checking is not None:
+        return -1
+    if solution is not None:
+        checking.update(**{"set__solution" : solution})
+
+    if resolution is not None:
+        checking.update(**{"set__resolution" : resolution})
+
+    if date_accept_ban is not None:
+        checking.update(**{"set__date_refuse_ban" : date_refuse_ban})
+
+    if date_refuse_ban is not None:
+        checking.update(**{"set__date_accept_ban" : date_accept_ban})
+
+    return 0
+
+
+def delete_checking(this_id):
+    checking = Checking.objects(id=this_id)
+    if checking is not None:
+        checking.delete()
+        return 0
+    else:
+        return -1
+
 
