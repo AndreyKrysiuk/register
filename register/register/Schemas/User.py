93ddc458756f4fda574c186a4f5c364b0528b511
AndreyKrysyuk <andrey130198@gmail.com>
2018-05-23 17:26:43 +0300

    Correct update

--- register/register/Schemas/User.py
+++ register/register/Schemas/User.py
@@ -3,7 +3,7 @@ from mongoengine import *
 
 class User(Document):
     login = StringField(max_length=30, required=True, min_length=6, unique=True)
-    password = StringField(max_length=30, required=True, min_length=9)
+    password = StringField(max_length=30, required=True, min_length=6)
     isBanned = BooleanField(required=True)
     isAdmin = BooleanField(required=True)
 
@@ -27,25 +27,24 @@ def add_new_user(login, password, is_banned = False, is_admin = False):
     return 0
 
 
-def update_user(login, password, is_banned, is_admin):
-    user = User.objects(login=login)
+def update_user(login, password, is_banned=False, is_admin=False):
+    user = User.objects(login=login)[0]
     if user is None:
         return -1
     if (len(password) >= 9) and (len(password) <= 30):
-        user.password = password
+        user.update(**{"set__password": password})
     else:
         return -1
 
     if is_banned is not None:
-        user.isBanned = is_banned
+        user.update(**{"set__isBanned": is_banned})
     if is_admin is not None:
-        user.isAdmin = is_admin
-
+        user.update(**{"set__isAdmin": is_admin})
     return 0
 
 
 def delete_user(login):
-    user = User.objects(login = login)
+    user = User.objects(login=login)
     if user is None:
         return -1
     else:
@@ -54,7 +53,7 @@ def delete_user(login):
 
 
 def get_user(login):
-    user = User.objects(login = login)
+    user = User.objects(login=login)[0]
     if user is None:
         return -1
     else:
