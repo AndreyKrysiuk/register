79f6e3a2a27a71e3692be00e4cb29e717caaaf1e
AndreyKrysyuk <andrey130198@gmail.com>
2018-05-23 19:41:32 +0300

    Person's CRUD again

--- register/register/Schemas/Person.py
+++ register/register/Schemas/Person.py
@@ -10,3 +10,65 @@ class Person(Document):
     isPretender = BooleanField(required=True)
 
 
+def add_new_person(name, category, job, position, region, isPretender=False):
+    person = Person()
+
+    person.name = name
+    person.category = category
+    person.job = job
+    person.position = position
+    person.region = region
+    if isPretender is not None:
+        person.isPretender = isPretender
+    else:
+        person.isPretender = False
+    person.save()
+    return 0
+
+
+def update_person(id, name, category, job, position, region, isPretender=False):
+
+    person = Person.objects(id=id)[0]
+    if person is None:
+        return -1
+
+    if name is not None:
+        person.update(**{"set__name": name})
+
+    if category is not None:
+        person.update(**{"set__category": category})
+
+    if job is not None:
+        person.update(**{"set__job": job})
+
+    if position is not None:
+        person.update(**{"set__position": position})
+
+    if region is not None:
+        person.update(**{"set__region": region})
+
+    if isPretender is not None:
+        person.update(**{"set__isPretender": isPretender})
+    return 0
+
+
+def delete_person(id):
+    person = Person.objects(id=id)
+    if person is None:
+        return -1
+    else:
+        person.delete()
+        return 0
+
+
+def get_person(id):
+    person = Person.objects(id=id)
+    if person is None:
+        return -1
+    else:
+        return person
+
+
+def get_all_persons():
+    return Person.objects()
+
